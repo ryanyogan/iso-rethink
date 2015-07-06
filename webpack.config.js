05b718d0726b58222fedecdc14b16ce463c5b158
Ryan Yogan <ryanyogan@gmail.com>
2015-07-05 20:51:26 -0500

    added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  entry: ['./client/app.jsx'],
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, loaders: ['babel', 'eslint'], include: __dirname + '/client' }
+    ]
+  }
+};
